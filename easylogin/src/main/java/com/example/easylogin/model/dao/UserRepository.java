0a7e53e8526b523bf933d2bc5f6d0d00feacdc36
sayaka-k71 <sayaka.kanomata@outlook.jp>
2023-02-24 12:25:39 +0900

    first commit

---
+++ easylogin/src/main/java/com/example/easylogin/model/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.easylogin.model.dao;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.easylogin.model.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	List<User> findByUserNameAndPassword(String userName, String password);
+
+}
